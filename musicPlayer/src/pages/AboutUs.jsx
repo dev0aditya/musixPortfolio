ec054e094fdd81e46e9f3210e6185ebca6682a7d
Aditya <maildevaditya@gmail.com>
2024-08-03 11:57:35 +0530

    added FAQ section to About Page

--- musicPlayer/src/pages/AboutUs.jsx
+++ musicPlayer/src/pages/AboutUs.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Aboutsection from "../component/Aboutsection";
+import ServicesSection from "../component/ServicesSection";
+import FaqSection from "../component/FaqSection";
 
 function AboutUs() {
-  return <Aboutsection />;
+  return (
+    <>
+      <Aboutsection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 }
 
 export default AboutUs;
